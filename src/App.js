f11217ec6e605cdf02644cb4768c07769327379e
Ayan Das <ayandas9531@gmail.com>
2022-12-09 19:39:35 +0530

    fetches news

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import axios, { formToJSON } from 'axios';
+import { useState, useEffect } from 'react';
+import { Headline } from './Components/Card';
+import { Nav } from './Components/Nav';
+
 
 function App() {
+
+
+  const [headline, setHeadline] = useState({});
+  const [headlineLoaded, setHeadlineLoaded] = useState(false);
+
+  useEffect(() => {
+    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=8a1bfd2f0ece4203809f4677f8ae4b9e").then(res => {
+      setHeadline(res.data);
+      setHeadlineLoaded(true);
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav /> 
+      {/* {headlineLoaded?<Headline news={headline} />:<h1>Loading</h1>} */}
     </div>
   );
 }
