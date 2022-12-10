3650bd0fe6390eefd61ef2820ba644dad55f1433
Ayan Das <ayandas9531@gmail.com>
2022-12-10 23:36:36 +0530

    fetches topic wise data and new state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import './App.css';
 import axios, { formToJSON } from 'axios';
 import { useState, useEffect } from 'react';
-import { Headline } from './Components/Card';
+import { Headline, AllNews } from './Components/Card';
 import { Nav } from './Components/Nav';
 
+function getNews(topic, setAllnews, setNewsLoaded) {
+    axios.get(`https://newsapi.org/v2/everything?q=${topic}&from=2022-12-09&to=2022-12-09&sortBy=popularity&apiKey=8a1bfd2f0ece4203809f4677f8ae4b9e`).then(res => {
+      setAllnews(res.data);
+      setNewsLoaded(true);
+    });
+}
 
 function App() {
 
 
   const [headline, setHeadline] = useState({});
   const [headlineLoaded, setHeadlineLoaded] = useState(false);
+  const [allnews, setAllnews] = useState({});
+  const [newsLoaded, setNewsLoaded] = useState(false)
+
+  const topic = 'Media';
+
+  
 
   useEffect(() => {
     axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=8a1bfd2f0ece4203809f4677f8ae4b9e").then(res => {
       setHeadline(res.data);
       setHeadlineLoaded(true);
     });
-  }, [])
+    getNews(topic, setAllnews, setNewsLoaded);
+ }, [])
 
   return (
     <div className="App">
-      <Nav /> 
-      {/* {headlineLoaded?<Headline news={headline} />:<h1>Loading</h1>} */}
+      <Nav />
+      <div className='Container'>
+        {headlineLoaded ? <Headline news={headline} /> : <h1>Loading</h1>}
+      {newsLoaded ? <AllNews news={allnews} topic={topic}/> : <h1>Loading</h1>}
+      </div>
     </div>
   );
 }
