12f60c2b1cff2fbcc54a5071adbef1bbbc5c61e6
Ayan Das <ayandas9531@gmail.com>
2022-12-22 20:08:19 +0530

    added: routing & components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react';
 import { Headline, AllNews } from './Components/Card';
 import { Nav } from './Components/Nav';
 import { Weather } from './Components/Weather';
+import './styles/loading.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Country } from './Components/Country';
 
 function getNews(topic, setNews, setNewsLoaded) {
   axios.get(`https://newsapi.org/v2/everything?q=${topic}&from=2022-12-09&to=2022-12-09&sortBy=popularity&apiKey=8a1bfd2f0ece4203809f4677f8ae4b9e`).then(res => {
@@ -24,7 +27,7 @@ function App() {
 
 
   useEffect(() => {
-    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=8a1bfd2f0ece4203809f4677f8ae4b9e").then(res => {
+    axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=8a1bfd2f0ece4203809f4677f8ae4b9e").then(res => {
       setHeadline(res.data);
       setHeadlineLoaded(true);
     });
@@ -34,12 +37,25 @@ function App() {
 
   return (
     <div className="App">
-      <Nav />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/Country' element={<Country />} />
+        </Routes>
+      </Router>
       <div className='Container'>
-        {headlineLoaded ? <Headline news={headline} /> : <h1>Loading</h1>}
-        {sportsLoaded ? <AllNews news={sportsNews} topic='Sports' /> : <h1>Loading</h1>}
-        < Weather city="London"/>
-        {politicsLoaded? <AllNews news={Politics} topic='Politics' /> : <h1>Loading</h1>}
+        <div className='a'>
+          {headlineLoaded ? <Headline news={headline} /> : <div className='news loading'></div>}
+        </div>
+        <div className='b'>
+          {sportsLoaded ? <AllNews news={sportsNews} topic='Sports' /> : <div className='news loading' />}
+        </div>
+        <div className='c'>
+          < Weather city="Port Blair" />
+        </div>
+        <div className='d'>
+          {politicsLoaded ? <AllNews news={Politics} topic='Politics' /> : <div className='news loading' />}
+        </div>
       </div>
     </div>
   );
