8dbae82afb8b17969c7986b560b2a6b71c3c28f2
Ayan Das <ayandas9531@gmail.com>
2022-12-23 20:41:17 +0530

    replaced 'class' with 'className'

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,14 +3,14 @@ import '../styles/card.css'
 export const Headline = (props) => {
     const articles = props.news.articles;
     return (
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
             <div><h6>HEADLINES</h6></div>
-            <div class="carousel-inner">
+            <div className="carousel-inner">
 
-                <div class="carousel-item active">
-                    <img src={articles[0].urlToImage} class="d-block w-80 carousel-image" />
-                    <a href={articles[0].url} target='_blank'>
-                        <div class="carousel-caption d-md-block" >
+                <div className="carousel-item active">
+                    <img src={articles[0].urlToImage} className="d-block w-80 carousel-image" alt='article background'/>
+                    <a href={articles[0].url} target='_blank' rel="noreferrer">
+                        <div className="carousel-caption d-md-block" >
                             <h5>{articles[0].title}</h5>
                             <p>{articles[0].description}</p>
                         </div>
@@ -19,10 +19,10 @@ export const Headline = (props) => {
 
                 {articles.slice(1).map((article, key) => {
                     return (
-                        <div class="carousel-item" key={key}>
-                            <img src={article.urlToImage} class="d-block w-80" />
-                            <a href={article.url} target='_blank'>
-                                <div class="carousel-caption d-md-block">
+                        <div className="carousel-item" key={key}>
+                            <img src={article.urlToImage} className="d-block w-80" alt='article background'/>
+                            <a href={article.url} target='_blank' rel="noreferrer">
+                                <div className="carousel-caption d-md-block">
                                     <h5>{article.title}</h5>
                                     <p>{article.description}</p>
                                 </div>
@@ -33,13 +33,13 @@ export const Headline = (props) => {
                 })}
 
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     )
@@ -54,9 +54,9 @@ export const AllNews = (props) => {
             <p className='tag'>{props.topic}</p>
             {articles.slice(0, noOfNews).map((article, key) => {
                 return (
-                    <a href={article.url} target="_blank" key={key}>
+                    <a href={article.url} target="_blank" key={key} rel="noreferrer">
                         <div className='article'>
-                            <img src={article.urlToImage} className='article-img' />
+                            <img src={article.urlToImage} className='article-img' alt='article cover icon'/>
                             <div className='info'>
                                 <h6>{article.title}</h6>
                             </div>
