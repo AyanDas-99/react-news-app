8cf5c0bd25c5aedbddca0cfd2f32d2ef92bac78c
Ayan Das <ayandas9531@gmail.com>
2022-12-24 01:03:43 +0530

    mila kya

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import { Headline, AllNews } from './Components/Card';
-import { Nav } from './Components/Nav';
-import { Weather } from './Components/Weather';
-import './styles/loading.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Country } from './Components/Country';
-import { useGetNews } from './Custom-hooks/useGetNews';
-
+import "./App.css";
+import { Headline, AllNews } from "./Components/Card";
+import { Nav } from "./Components/Nav";
+import { Weather } from "./Components/Weather";
+import "./styles/loading.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Country } from "./Components/Country";
+import { useGetNews } from "./Custom-hooks/useGetNews";
 
 function App() {
-
   const [Politics, politicsLoaded] = useGetNews("politics", true);
   const [sportsNews, sportsLoaded] = useGetNews("sports", true);
   const [headline, headlineLoaded] = useGetNews("", false);
@@ -19,22 +17,34 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path='/Country' element={<Country />} />
-          <Route path='*' element={<></>} />
+          <Route path="/Country" element={<Country />} />
+          <Route path="*" element={<></>} />
         </Routes>
       </Router>
-      <div className='Container'>
-        <div className='a'>
-          {headlineLoaded ? <Headline news={headline} /> : <div className='news loading'></div>}
+      <div className="Container">
+        <div className="a">
+          {headlineLoaded ? (
+            <Headline news={headline} />
+          ) : (
+            <div className="news loading"></div>
+          )}
         </div>
-        <div className='b'>
-          {sportsLoaded ? <AllNews news={sportsNews} topic='Sports' /> : <div className='news loading' />}
+        <div className="b">
+          {sportsLoaded ? (
+            <AllNews news={sportsNews} topic="Sports" />
+          ) : (
+            <div className="news loading" />
+          )}
         </div>
-        <div className='c'>
-          < Weather city="Port Blair" />
+        <div className="c">
+          <Weather city="Port Blair" />
         </div>
-        <div className='d'>
-          {politicsLoaded ? <AllNews news={Politics} topic='Politics' /> : <div className='news loading' />}
+        <div className="d">
+          {politicsLoaded ? (
+            <AllNews news={Politics} topic="Politics" />
+          ) : (
+            <div className="news loading" />
+          )}
         </div>
       </div>
     </div>
