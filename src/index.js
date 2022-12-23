f5d3ae15e01bd169a68f7491b8307335c8a70a98
Ayan Das <ayandas9531@gmail.com>
2022-12-23 20:15:50 +0530

    made react-query available to App.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new QueryClient();
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
