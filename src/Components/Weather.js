cc4ea651f20ee90776aee1799f87fd4622945651
Ayan Das <ayandas9531@gmail.com>
2022-12-23 20:41:49 +0530

    replaced useEffect & useState with react-query

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,56 +1,63 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import wind from '../media/wind.png';
-import cloud from '../media/cloud_sun.png';
-import drops from '../media/drops.png';
-import therm from '../media/thermometer.png'
-import loc from '../media/loc.png'
-import '../styles/weather.css'
+import axios from "axios";
+import wind from "../media/wind.png";
+import cloud from "../media/cloud_sun.png";
+import drops from "../media/drops.png";
+import therm from "../media/thermometer.png";
+import loc from "../media/loc.png";
+import "../styles/weather.css";
 import { Loading } from "./Loading";
-import '../styles/loading.css'
+import "../styles/loading.css";
+import { useQuery } from "react-query";
 
 export const Weather = (props) => {
+  const options = {
+    method: "GET",
+    url: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
+    params: { city: props.city },
+    headers: {
+      "X-RapidAPI-Key": "e0b6052bacmshcfe5da2fca3f2e5p14d484jsn5c655e5224c5",
+      "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    },
+  };
 
-    const [weather, setWeather] = useState({})
-    const [weatherLoaded, setLoaded] = useState(false)
+  const {
+    data: weather,
+    isLoading,
+    isError,
+  } = useQuery(["weather"], async () => {
+    return axios.request(options).then((res) => res.data);
+  });
 
-    const options = {
-        method: 'GET',
-        url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
-        params: { city: props.city },
-        headers: {
-            'X-RapidAPI-Key': 'e0b6052bacmshcfe5da2fca3f2e5p14d484jsn5c655e5224c5',
-            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-        }
-    };
-
-    useEffect(() => {
-        axios.request(options).then(function (response) {
-            setWeather(response.data)
-            setLoaded(true)
-            console.log(weather)
-        }).catch(function (error) {
-            console.error(error);
-        });
-    }, [])
-
-    if (weatherLoaded) {
-
-        return (
-            <div className="weather">
-                <p className="city"><img src={loc} />{props.city}</p>
-                <div className="hero">
-                    <div className="temp"><img src={cloud} /><span>{weather.temp}</span>&#8451;</div>
-                    <div className="other">
-                        <p><img src={therm} />Feels like {weather.feels_like}</p>
-                        <p><img src={drops} />Humidity: {weather.humidity}</p>
-                        <p><img src={wind} />Wind: {weather.wind_speed}</p>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-    else {
-        <div className="weather loading"></div>
-    }
-}
+  if (!isLoading && !isError) {
+    return (
+      <div className="weather">
+        <p className="city">
+          <img src={loc} alt="location icon"/>
+          {props.city}
+        </p>
+        <div className="hero">
+          <div className="temp">
+            <img src={cloud} alt="cloud icon"/>
+            <span>{weather.temp}</span>&#8451;
+          </div>
+          <div className="other">
+            <p>
+              <img src={therm} alt="thermometer icon"/>
+              Feels like {weather.feels_like}
+            </p>
+            <p>
+              <img src={drops} alt="water drop icon"/>
+              Humidity: {weather.humidity}
+            </p>
+            <p>
+              <img src={wind} alt="wind blowing icon"/>
+              Wind: {weather.wind_speed}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    <Loading />
+  }
+};
