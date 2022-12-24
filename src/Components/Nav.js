860249e7fc6658ce9efd4c55e693e3028df52e03
Ayan Das <ayandas9531@gmail.com>
2022-12-25 00:13:32 +0530

    changes country name state to respective country name

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,25 +3,28 @@ import '../styles/nav.css'
 import hamMenuIcon from '../media/ham-menu.png'
 import crossIcon from '../media/cross.png'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../App'
+import { useContext } from 'react'
 
 export const Nav = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const {setCountry} = useContext(AppContext);
 
     return (
         <nav className='primary-nav'>
-            <img src={hamMenuIcon} id="menu-btn" onClick={() => setShowMenu(true)} alt=""/>
+            <img src={hamMenuIcon} id="menu-btn" onClick={() => setShowMenu(true)} alt="" />
             <h1 className='heading'>FAKES<span className='red'>.</span><span className='small'>com</span></h1>
             <div className={`links ${showMenu && "showmenu"}`}>
                 <div className={`link-list`}>
-                <img src={crossIcon} onClick={() => setShowMenu(false)} alt=""/>
+                    <img src={crossIcon} onClick={() => setShowMenu(false)} alt="" />
                     <a href='/feed'>HOME</a>
-                    <a href='/Country'>HEADLINES</a>
+                    <Link to={'/Country'} onClick={() => setCountry("")}>HEADLINES</Link>
                     <div className='divider'></div>
-                    <Link to={'/Country'}>UNITED STATES</Link>
-                    <a href='/Country'>INDIA</a>
-                    <a href='/Country'>BRITAIN</a>
-                    <a href='/Country'>RUSSIAN</a>
-                    <a href='/Country`'>EUROPE</a>
+                    <Link to={'/Country'} onClick={()=>setCountry("us")}>UNITED STATES</Link>
+                    <Link to={'/Country'} onClick={() => setCountry("in")}>INDIA</Link>
+                    <Link to={'/Country'} onClick={() => setCountry("br")}>BRITAIN</Link>
+                    <Link to={'/Country'} onClick={() => setCountry("ru")}>RUSSIAN</Link>
+                    <Link to={'/Country'} onClick={()=>setCountry("sa")}>SOUTH AFRICA</Link>
                 </div>
             </div>
         </nav>
