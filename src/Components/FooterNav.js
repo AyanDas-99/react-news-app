a5726e8f5e97a1fb71c05ea26d65959ca97d5b6c
Ayan Das <ayandas9531@gmail.com>
2022-12-25 00:13:15 +0530

    changes country name state to the respective country name'

--- src/Components/FooterNav.js
+++ src/Components/FooterNav.js
@@ -1,17 +1,21 @@
 import styles from '../styles/footer.module.css'
+import { useContext } from 'react'
+import { AppContext } from '../App'
+import { Link } from 'react-router-dom';
 
 export const FooterNav = () => {
+    const {setCountry} = useContext(AppContext);
     return (
         <footer className={styles.bottomNav}>
             <div className={styles.container}>
                 <div className={styles.main}>
                     <div className={styles.countries + ' ' + styles.list}>
                         <ul>
-                            <a href='/Country'>United States</a>
-                            <a href='/Country'>India</a>
-                            <a href='/Country'>Britain</a>
-                            <a href='/Country'>Russia</a>
-                            <a href='/Country'>Europe</a>
+                            <Link to={'/Country'} onClick={()=>setCountry("us")}>United States</Link>
+                            <Link to={'/Country'}  onClick={()=>setCountry("in")}>India</Link>
+                            <Link to={'/Country'}  onClick={()=>setCountry("br")}>Britain</Link>
+                            <Link to={'/Country'}  onClick={()=>setCountry("ru")}>Russia</Link>
+                            <Link to={'/Country'}  onClick={()=>setCountry("sa")}>South Africa</Link>
                         </ul>
                     </div>
                     <div className={styles.pages + ' ' + styles.list}>
