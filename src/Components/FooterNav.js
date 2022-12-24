7fb55ae0a5a159e960a49b49074137c3f24fe2d2
Ayan Das <ayandas9531@gmail.com>
2022-12-24 23:05:30 +0530

    added footer links

--- src/Components/FooterNav.js
+++ src/Components/FooterNav.js
@@ -1,18 +1,32 @@
-import '../styles/footer.css'
+import styles from '../styles/footer.module.css'
 
 export const FooterNav = () => {
     return (
-        <footer className='footer-nav'>
-            <div className='container'>
-                <div className='logo'>FAKES<span>.</span>com</div>
-                <div className='pages'>
-                    <ul>
-                        <a href='/Country'>UNITED STATES</a>
-                        <a href='/Country'>INDIA</a>
-                        <a href='/Country'>BRITAIN</a>
-                        <a href='/Country'>RUSSIAN</a>
-                        <a href='/Country`'>EUROPE</a>
-                    </ul>
+        <footer className={styles.bottomNav}>
+            <div className={styles.container}>
+                <div className={styles.main}>
+                    <div className={styles.countries + ' ' + styles.list}>
+                        <ul>
+                            <a href='/Country'>United States</a>
+                            <a href='/Country'>India</a>
+                            <a href='/Country'>Britain</a>
+                            <a href='/Country'>Russia</a>
+                            <a href='/Country'>Europe</a>
+                        </ul>
+                    </div>
+                    <div className={styles.pages + ' ' + styles.list}>
+                        <ul>
+                            <a href='/feed'>Home</a>
+                            <a href='/Contact'>Contact Us</a>
+                            <a href='/Request'>Request Media Coverage</a>
+                            <a href='/About'>About Us</a>
+                            <a href='/Sponser'>Sponser Us</a>
+                        </ul>
+                    </div>
+                    <div className={styles.logo}><a href='/feed'>FAKES<span>.</span>com</a></div>
+                </div>
+                <div className={styles.footer}>
+                    Copyrights &#169; Fakes.com
                 </div>
             </div>
         </footer>
