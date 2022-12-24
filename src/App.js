d4d90972003c9f0f8f36583368515f35c4be6ed8
Ayan Das <ayandas9531@gmail.com>
2022-12-25 00:08:31 +0530

    created context for country news variable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import "./App.css";
 import { Nav } from "./Components/Nav";
 import "./styles/loading.css";
-import { BrowserRouter as Router, Route, Routes , Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Country } from "./Components/Country";
 import { Feed } from "./pages/Feed";
 import { FooterNav } from "./Components/FooterNav";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [country, setCountry] = useState('');
   return (
     <div className="App">
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Navigate to="/feed" />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/Country" element={<Country />} />
-          <Route path="*" element={<h1>It's the wrong page</h1>} />
-        </Routes>
-        <FooterNav />
-      </Router>
-   </div>
+      <AppContext.Provider value={{ country, setCountry }}>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Navigate to="/feed" />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/Country" element={<Country />} />
+            <Route path="*" element={<h1>It's the wrong page</h1>} />
+          </Routes>
+          <FooterNav />
+        </Router>
+      </AppContext.Provider>
+    </div>
   );
 }
 
