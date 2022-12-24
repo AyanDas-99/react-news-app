c196c4999a2bf610e15fa46ba770d912b41252cb
Ayan Das <ayandas9531@gmail.com>
2022-12-24 10:14:06 +0530

    removed Home page component and shifted to feed page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
 import "./App.css";
-import { Headline, AllNews } from "./Components/Card";
 import { Nav } from "./Components/Nav";
-import { Weather } from "./Components/Weather";
 import "./styles/loading.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes , Navigate} from "react-router-dom";
 import { Country } from "./Components/Country";
-import { useGetNews } from "./Custom-hooks/useGetNews";
+import { Feed } from "./pages/Feed";
 
 function App() {
-  const [Politics, politicsLoaded] = useGetNews("politics", true);
-  const [sportsNews, sportsLoaded] = useGetNews("sports", true);
-  const [headline, headlineLoaded] = useGetNews("", false);
-
   return (
     <div className="App">
       <Router>
         <Nav />
         <Routes>
+          <Route path="/" element={<Navigate to="/feed" />} />
+          <Route path="/feed" element={<Feed />} />
           <Route path="/Country" element={<Country />} />
-          <Route path="*" element={<></>} />
+          <Route path="*" element={<h1>It's the wrong page</h1>} />
         </Routes>
       </Router>
-      <div className="Container">
-        <div className="a">
-          {headlineLoaded ? (
-            <Headline news={headline} />
-          ) : (
-            <div className="news loading"></div>
-          )}
-        </div>
-        <div className="b">
-          {sportsLoaded ? (
-            <AllNews news={sportsNews} topic="Sports" />
-          ) : (
-            <div className="news loading" />
-          )}
-        </div>
-        <div className="c">
-          <Weather city="Port Blair" />
-        </div>
-        <div className="d">
-          {politicsLoaded ? (
-            <AllNews news={Politics} topic="Politics" />
-          ) : (
-            <div className="news loading" />
-          )}
-        </div>
-      </div>
-    </div>
+   </div>
   );
 }
 
