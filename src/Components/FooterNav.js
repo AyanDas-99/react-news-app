769adac6fc46c3974b3e7b7062a8f84cb3e5a546
Ayan Das <ayandas9531@gmail.com>
2022-12-25 02:33:16 +0530

    a tags to Link tags

--- src/Components/FooterNav.js
+++ src/Components/FooterNav.js
@@ -4,30 +4,30 @@ import { AppContext } from '../App'
 import { Link } from 'react-router-dom';
 
 export const FooterNav = () => {
-    const {setCountry} = useContext(AppContext);
+    const { setCountry } = useContext(AppContext);
     return (
         <footer className={styles.bottomNav}>
             <div className={styles.container}>
                 <div className={styles.main}>
                     <div className={styles.countries + ' ' + styles.list}>
                         <ul>
-                            <Link to={'/Country'} onClick={()=>setCountry("us")}>United States</Link>
-                            <Link to={'/Country'}  onClick={()=>setCountry("in")}>India</Link>
-                            <Link to={'/Country'}  onClick={()=>setCountry("br")}>Britain</Link>
-                            <Link to={'/Country'}  onClick={()=>setCountry("ru")}>Russia</Link>
-                            <Link to={'/Country'}  onClick={()=>setCountry("sa")}>South Africa</Link>
+                            <Link to={'/Country'} onClick={() => setCountry("us")}>United States</Link>
+                            <Link to={'/Country'} onClick={() => setCountry("in")}>India</Link>
+                            <Link to={'/Country'} onClick={() => setCountry("br")}>Britain</Link>
+                            <Link to={'/Country'} onClick={() => setCountry("ru")}>Russia</Link>
+                            <Link to={'/Country'} onClick={() => setCountry("sa")}>South Africa</Link>
                         </ul>
                     </div>
                     <div className={styles.pages + ' ' + styles.list}>
                         <ul>
-                            <a href='/feed'>Home</a>
-                            <a href='/Contact'>Contact Us</a>
-                            <a href='/Request'>Request Media Coverage</a>
-                            <a href='/About'>About Us</a>
-                            <a href='/Sponser'>Sponser Us</a>
+                            <Link to={'/feed'}>Home</Link>
+                            <Link to={'/Contact'}>Contact Us</Link>
+                            <Link to={'/Request'}>Request Media Coverage</Link>
+                            <Link to={'/About'}>About Us</Link>
+                            <Link to={'/Sponser'}>Sponser Us</Link>
                         </ul>
                     </div>
-                    <div className={styles.logo}><a href='/feed'>FAKES<span>.</span>com</a></div>
+                    <div className={styles.logo}><Link to={'/feed'}>FAKES<span>.</span>com</Link></div>
                 </div>
                 <div className={styles.footer}>
                     Copyrights &#169; Fakes.com
